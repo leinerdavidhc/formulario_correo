08cb2cadccab24055e3fb0ebf69d54daa9df0a8e
leinerdavidhc <107592704+leinerdavidhc@users.noreply.github.com>
2022-12-10 01:10:45 -0500

    Update correo.php

---
+++ correo.php
@@ -0,0 +1,12 @@
+<?php
+if (isset($_POST['enviar'])) {
+    if(!empty($_POST['email'])&& !empty($_POST['mensaje'])){
+        $email = $_POST['email'];
+        $mensaje = $_POST['mensaje'];
+        $header = "From: noreply@example.com" . "\r\n";
+        $header.= "Reply-To noreply@example.com" . "\r\n";
+        $header .= "X-Mailer: PHP/". phpversion();
+        $mail=@mail($email, $mensaje, $header);
+    }
+
+}
